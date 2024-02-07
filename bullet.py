ba9e73520fe1a11b26b293f63a85ad20e0b79484
v <v8polev@gmail.com>
2024-02-07 18:15:51 +0500

    fool game

--- bullet.py
+++ bullet.py
@@ -1,27 +1,22 @@
 import pygame
-from pygame.sprite import AbstractGroup
 
 class Bullet(pygame.sprite.Sprite):
-    
-    def __init__(self, screan, gun):
+
+    def __init__(self, screen, gun):
         super(Bullet, self).__init__()
-        self.screen = screan
+        self.screen = screen
         self.rect = pygame.Rect(0, 0, 2, 12)
-        self.color = 139, 195, 74
-        self.speed = 1.5
+        self.color = 14, 179, 229
+        self.speed = 4.5
         self.rect.centerx = gun.rect.centerx
         self.rect.top = gun.rect.top
         self.y = float(self.rect.y)
-        
+
     def update(self):
+        """перемещение пули вверх"""
         self.y -= self.speed
         self.rect.y = self.y
-        
-        
-    def drow_bullet(self):
+
+    def draw_bullet(self):
+        """рисуем пулю на экране"""
         pygame.draw.rect(self.screen, self.color, self.rect)
-        
-        
-        
-        
-        
--- gun.py
+++ gun.py
@@ -1,11 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Gun():
-    
+class Gun(Sprite):
+
     def __init__(self, screen):
-        
+        super(Gun, self).__init__()
         self.screen = screen
-        self.image = pygame.image.load("game/images/gun1.png")
+        self.image = pygame.image.load('game/images/gun1.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
@@ -13,27 +14,18 @@ class Gun():
         self.rect.bottom = self.screen_rect.bottom
         self.mright = False
         self.mleft = False
-        
-        
+
     def output(self):
-        
         self.screen.blit(self.image, self.rect)
-    
+
     def update_gun(self):
-        
         if self.mright and self.rect.right < self.screen_rect.right:
             self.center += 1.5
         if self.mleft and self.rect.left > 0:
             self.center -= 1.5
-            
+
         self.rect.centerx = self.center
-            
-            
-            
-            
-            
-            
-            
-        
-        
-        
+
+    def create_gun(self):
+        """размещение пушки по центру внизу экрана"""
+        self.center = self.screen_rect.centerx
